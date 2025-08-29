f4774d12dd2b1469ca9452b344a93035a6037127
DesaiLaya <117029556+DesaiLaya@users.noreply.github.com>
2025-08-29 11:03:42 +0530

    Update WebConfig.java

--- employee-login-backend2 (13)/employee-login-backend2 (6)/employee-login-backend2/src/main/java/com/register/example/config/WebConfig.java
+++ employee-login-backend2 (13)/employee-login-backend2 (6)/employee-login-backend2/src/main/java/com/register/example/config/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**") // allow all /api/ requests
-                        .allowedOrigins("http://localhost:3000") // allow React app
+                     .allowedOrigins("http://localhost:3000", "http://172.31.12.229:3000")  // allow React app
                         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);

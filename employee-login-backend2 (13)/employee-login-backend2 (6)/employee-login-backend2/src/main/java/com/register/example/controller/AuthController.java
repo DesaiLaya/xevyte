01aef670d350b65586cfa91886b93a32c62ec77f
DesaiLaya <117029556+DesaiLaya@users.noreply.github.com>
2025-08-29 18:32:20 +0530

    Update AuthController.java

--- employee-login-backend2 (13)/employee-login-backend2 (6)/employee-login-backend2/src/main/java/com/register/example/controller/AuthController.java
+++ employee-login-backend2 (13)/employee-login-backend2 (6)/employee-login-backend2/src/main/java/com/register/example/controller/AuthController.java
@@ -23,7 +23,7 @@ import java.util.UUID;
  
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = {"http://localhost:3000", "http://65.2.74.194"})
+@CrossOrigin(origins = {"http://localhost:3000", "http://65.2.74.194:3000"})
 public class AuthController {
  
     @Autowired
